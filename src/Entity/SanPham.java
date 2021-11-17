08e4ae56389ff9887062465e99f3ec6702a437de
Nam Thành <94166771+tbnsbang000@users.noreply.github.com>
2021-11-17 21:57:14 +0700

    ThemEntitySanPham

---
+++ src/Entity/SanPham.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class SanPham {
+    int maSanPham;
+    int maLoaiSanPham;
+    String tenSanPham;
+    int donGia;
+    String donViTinh;
+
+    public int getMaSanPham() {
+        return maSanPham;
+    }
+
+    public int getMaLoaiSanPham() {
+        return maLoaiSanPham;
+    }
+
+    public String getTenSanPham() {
+        return tenSanPham;
+    }
+
+    public int getDonGia() {
+        return donGia;
+    }
+
+    public String getDonViTinh() {
+        return donViTinh;
+    }
+
+    public void setMaSanPham(int maSanPham) {
+        this.maSanPham = maSanPham;
+    }
+
+    public void setMaLoaiSanPham(int maLoaiSanPham) {
+        this.maLoaiSanPham = maLoaiSanPham;
+    }
+
+    public void setTenSanPham(String tenSanPham) {
+        this.tenSanPham = tenSanPham;
+    }
+
+    public void setDonGia(int donGia) {
+        this.donGia = donGia;
+    }
+
+    public void setDonViTinh(String donViTinh) {
+        this.donViTinh = donViTinh;
+    }
+    
+    
+           
+            
+}
