b0a80c39092a971210bcb7ab44cdaac5478501b8
Tạ Đình Long PH 1 7 8 5 0 <longtdph17850@fpt.edu.vn>
2021-12-08 11:15:19 +0700

    sua lai

--- src/Helper/JDBC.java
+++ src/Helper/JDBC.java
@@ -24,10 +24,11 @@ import javax.swing.JOptionPane;
  * @author nguye
  */
 public class JDBC {
+
     static String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    static String dburl = "jdbc:sqlserver://localhost:1433;databaseName=QuanLyTraSua3";
+    static String dburl = "jdbc:sqlserver://localhost:1433;databaseName=QuanLyTraSua10";
     static String user = "sa";
-    static String pass = "123456";
+    static String pass = "123";
 
     //Nap Driver
 //    static {
@@ -37,7 +38,6 @@ public class JDBC {
 //            throw new RuntimeException(e);
 //        }
 //    }
-
     //Xjdbc.GETSTMT()
     public static PreparedStatement getStmt(String sql, Object... args) throws SQLException {
         Connection conn = DriverManager.getConnection(dburl, user, pass);
@@ -88,183 +88,190 @@ public class JDBC {
             throw new RuntimeException(e);
         }
     }
-    
-    
+
     private Connection cn;
-    
-    public JDBC(){
+
+    public JDBC() {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=QuanLyTraSua3";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=QuanLyTraSua10";
             String s = System.getProperty("os.name");
-            if(s.contains("Windows")){
-                cn = DriverManager.getConnection(url, "sa", "123456");
-            }else{
-                cn = DriverManager.getConnection(url, "sa", "123456");
+            if (s.contains("Windows")) {
+                cn = DriverManager.getConnection(url, "sa", "123");
+            } else {
+                cn = DriverManager.getConnection(url, "sa", "123");
             }
         } catch (Exception e) {
             e.printStackTrace();
-        }       
+        }
     }
-    
-        public hoaDonCT GetDsChiTiet(String ma, int maban){
+
+    public hoaDonCT GetDsChiTiet(String ma, int maban) {
         hoaDonCT arrDs = null;
         String sql;
 
-            sql = "Select SoLuong, Gia, MaChiTietHD From chitiethd AS ct INNER JOIN hoadon AS hd ON ct.MaHoaDon = hd.MaHoaDon Where MaMon = '"+ma+"' AND MaBan = '"+maban+"' AND hd.TrangThai = 0";
-        try{
+        sql = "Select SoLuong, Gia, MaChiTietHD From chitiethd AS ct INNER JOIN hoadon AS hd ON ct.MaHoaDon = hd.MaHoaDon Where MaMon = '" + ma + "' AND MaBan = '" + maban + "' AND hd.TrangThai = 0";
+        try {
             Statement st = cn.createStatement();
             ResultSet rs = st.executeQuery(sql);
-            while(rs.next()){
+            while (rs.next()) {
                 arrDs = new hoaDonCT();
                 arrDs.setSoLuong(rs.getInt(1));
                 arrDs.setGia(rs.getInt(2));
                 arrDs.setMaHoaDonCT(rs.getInt(3));
             }
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Không lấy được danh sách Order !");
         }
-        return arrDs;        
-    } 
-    
-    public int GetMaHD(int ma){
+        return arrDs;
+    }
+
+    public int GetMaHD(int ma) {
         String sql;
         int mahd = 0;
-            sql = "Select MaHoaDon From hoadon Where MaBan = '"+ma+"' AND TrangThai = 0";
-        try{
+        sql = "Select MaHoaDon From hoadon Where MaBan = '" + ma + "' AND TrangThai = 0";
+        try {
             Statement st = cn.createStatement();
             ResultSet rs = st.executeQuery(sql);
-            while(rs.next()){
+            while (rs.next()) {
                 mahd = rs.getInt(1);
             }
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
-        return mahd;        
-    } 
-        public int InsertHoaDon(HoaDon hd, String gio){
+        return mahd;
+    }
+
+    public int InsertHoaDon(HoaDon hd, String gio) {
         int insert = 0;
-        String sql = "Insert into hoadon (MaBan, NgayDen, TrangThai) values ('"+hd.getMaBan()+"', '"+gio+"', '"+hd.getTrangThai()+"')";
-        try{
+        String sql = "Insert into hoadon (MaBan, NgayDen, TrangThai) values ('" + hd.getMaBan() + "', '" + gio + "', '" + hd.getTrangThai() + "')";
+        try {
             Statement st = cn.createStatement();
             insert = st.executeUpdate(sql);
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
         }
         return insert;
     }
-    
-    public int UpdateChiTiet(hoaDonCT ct){
+
+    public int UpdateChiTiet(hoaDonCT ct) {
         int update = 0;
-        String sql = "UPDATE chitiethd SET SoLuong = '"+ct.getSoLuong()+"', Gia = '"+ct.getGia()+"' WHERE MaChiTietHD = '"+ct.getMaHoaDonCT()+"'";
-        try{
+        String sql = "UPDATE chitiethd SET SoLuong = '" + ct.getSoLuong() + "', Gia = '" + ct.getGia() + "' WHERE MaChiTietHD = '" + ct.getMaHoaDonCT() + "'";
+        try {
             Statement st = cn.createStatement();
             update = st.executeUpdate(sql);
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Update chi tiết không thành công !");
         }
-        return update;        
+        return update;
     }
-    public int InsertChiTietHD(hoaDonCT cthd){
+
+    public int InsertChiTietHD(hoaDonCT cthd) {
         int insert = 0;
-        String sql = "Insert into chitiethd (MaHoaDon, MaMon, SoLuong, Gia) values ('"+cthd.getMaHoaDon()+"', '"+cthd.getMaMon()+"', '"+cthd.getSoLuong()+"', '"+cthd.getGia()+"')";
-        try{
+        String sql = "Insert into chitiethd (MaHoaDon, MaMon, SoLuong, Gia) values ('" + cthd.getMaHoaDon() + "', '" + cthd.getMaMon() + "', '" + cthd.getSoLuong() + "', '" + cthd.getGia() + "')";
+        try {
             Statement st = cn.createStatement();
             insert = st.executeUpdate(sql);
-        }catch(SQLException ex){
-            JOptionPane.showMessageDialog(null, "Thêm chi tiết hóa đơn không thành công !"+ex.toString());
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Thêm chi tiết hóa đơn không thành công !" + ex.toString());
         }
         return insert;
     }
-        public int UpdateBan(Ban b){
+
+    public int UpdateBan(Ban b) {
         int update = 0;
-        String sql = "UPDATE ban SET TenBan = '"+b.getTenBan()+"', TrangThai = '"+b.getTrangThai()+"' WHERE MaBan = '"+b.getMaBan()+"'";
-        try{
+        String sql = "UPDATE ban SET TenBan = '" + b.getTenBan() + "', TrangThai = '" + b.getTrangThai() + "' WHERE MaBan = '" + b.getMaBan() + "'";
+        try {
             Statement st = cn.createStatement();
             update = st.executeUpdate(sql);
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Update bàn không thành công !");
         }
         return update;
-    } 
-    public ArrayList<DsOrder> GetDsOrder(int ma){
+    }
+
+    public ArrayList<DsOrder> GetDsOrder(int ma) {
         ArrayList<DsOrder> arrDs = null;
         String sql;
-            sql = "Select ct.MaMon, TenMon, DVT, SoLuong, Gia, MaHoaDon From chitiethd AS ct INNER JOIN SanPham AS td ON ct.MaMon = td.MaMon Where ct.MaHoaDon = '"+ma+"'";
-        try{
+        sql = "Select ct.MaMon, TenMon, DVT, SoLuong, Gia, MaHoaDon From chitiethd AS ct INNER JOIN SanPham AS td ON ct.MaMon = td.MaMon Where ct.MaHoaDon = '" + ma + "'";
+        try {
             Statement st = cn.createStatement();
             ResultSet rs = st.executeQuery(sql);
             arrDs = new ArrayList<DsOrder>();
-            while(rs.next()){
+            while (rs.next()) {
                 DsOrder order = new DsOrder(rs.getString(2), rs.getString(1), rs.getString(3), rs.getInt(5), rs.getInt(4), rs.getInt(6));
                 arrDs.add(order);
             }
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Không lấy được danh sách Order !");
         }
-        return arrDs;        
-    }    
-    public HoaDon GetHDbyMaBan(int ma){
+        return arrDs;
+    }
+
+    public HoaDon GetHDbyMaBan(int ma) {
         String sql;
         HoaDon arrhd = null;
-            sql = "Select * From hoadon Where MaBan = '"+ma+"' AND TrangThai = 0";
-        try{
+        sql = "Select * From hoadon Where MaBan = '" + ma + "' AND TrangThai = 0";
+        try {
             Statement st = cn.createStatement();
             ResultSet rs = st.executeQuery(sql);
-            while(rs.next()){
+            while (rs.next()) {
                 arrhd = new HoaDon(rs.getInt(1), rs.getInt(3), rs.getInt(2), rs.getInt(5), rs.getInt(6), rs.getTimestamp(4));
             }
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
-        return arrhd;        
-    }   
-        public ArrayList<SanPham> GetThucDonByMa(String ten){
+        return arrhd;
+    }
+
+    public ArrayList<SanPham> GetThucDonByMa(String ten) {
         ArrayList<SanPham> arrThucDon = null;
         String sql;
 
-            sql = "Select * From SanPham Where MaMon = '"+ten+"'";
-        try{
+        sql = "Select * From SanPham Where MaMon = '" + ten + "'";
+        try {
             Statement st = cn.createStatement();
             ResultSet rs = st.executeQuery(sql);
             arrThucDon = new ArrayList<SanPham>();
-            while(rs.next()){
+            while (rs.next()) {
                 SanPham td = new SanPham(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getInt(4), rs.getString(5));
                 arrThucDon.add(td);
             }
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Không lấy được danh sách thực đơn !");
         }
-        return arrThucDon;        
+        return arrThucDon;
     }
-            public int DeleteMon(String mamon, int mahd, int maban){
+
+    public int DeleteMon(String mamon, int mahd, int maban) {
         int check = 0;
-        try{
+        try {
             String sql;
-            sql = "Delete From chitiethd Where MaMon = '"+mamon+"' AND MaHoaDon = '"+mahd+"'";
+            sql = "Delete From chitiethd Where MaMon = '" + mamon + "' AND MaHoaDon = '" + mahd + "'";
             Statement st = cn.createStatement();
             st.executeUpdate(sql);
             check = 1;
-            if(CheckDsMon(mahd, maban) == 0){
+            if (CheckDsMon(mahd, maban) == 0) {
                 check = 2;
             }
-        }catch(SQLException ex){
-            
+        } catch (SQLException ex) {
+
         }
         return check;
     }
-                public int CheckDsMon(int mahd, int maban){
+
+    public int CheckDsMon(int mahd, int maban) {
         String sql;
         int dem = 0;
-            sql = "Select * From hoadon AS hd INNER JOIN chitiethd AS ct ON ct.MaHoaDon = hd.MaHoaDon Where MaBan = '"+maban+"' AND ct.MaHoaDon = '"+mahd+"' AND TrangThai = 0";
-        try{
+        sql = "Select * From hoadon AS hd INNER JOIN chitiethd AS ct ON ct.MaHoaDon = hd.MaHoaDon Where MaBan = '" + maban + "' AND ct.MaHoaDon = '" + mahd + "' AND TrangThai = 0";
+        try {
             Statement st = cn.createStatement();
             ResultSet rs = st.executeQuery(sql);
-            while(rs.next()){
+            while (rs.next()) {
                 dem++;
             }
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Không lấy được danh sách hóa đơn !");
         }
-        return dem;        
+        return dem;
     }
 }
